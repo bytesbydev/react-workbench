0d26a0f201ed9c1772099017432992a48a639cb5
devendrarawat-code <devendrarawat9876@gmail.com>
2026-03-18 20:00:00 +0530

    made some minor changes

--- src/useeffect hook/Question2.jsx
+++ src/useeffect hook/Question2.jsx
@@ -15,8 +15,11 @@ const Question2 = () => {
   return (
     <div>
       <h2>
-{time==0?"Times up":`Time is ${time}`}
+{time === 0 ? "Time's Up!" : `Time is ${time}`}
 </h2>
+<button onClick={() => setTime(10)}>
+  Reset
+</button>
     </div>
   )
 }
