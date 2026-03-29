09147834fafaee6e68bc7440325126222f9e49f2
devendrarawat-code <devendrarawat9876@gmail.com>
2026-03-29 20:00:00 +0530

    Update project files

--- src/useRef hook/Question3.jsx
+++ src/useRef hook/Question3.jsx
@@ -3,7 +3,10 @@ import React, { useRef, useState } from 'react'
 const Question3 = () => {
     const[timer,setTimer]=useState(0)
     const intervalRef=useRef(null)
-
+const reset = () => {
+  clearInterval(intervalRef.current);
+  setTimer(0);
+};
 
     const start=()=>{
       clearInterval(intervalRef.current)
@@ -17,6 +20,7 @@ return (
       {String(timer).padStart(2,"0")}
 <button onClick={start}>start</button>
 <button onClick={stop}>stop </button>
+<button onClick={reset}>Reset</button>
     </div>
   )
 }
