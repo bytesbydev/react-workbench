d452e8c484fbb459b1f97ce9ec2ab715022a14b8
devendrarawat-code <devendrarawat9876@gmail.com>
2026-03-29 20:00:00 +0530

    Update project files

--- src/useRef hook/Question4.jsx
+++ src/useRef hook/Question4.jsx
@@ -1,22 +1,27 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
 const Question4 = () => {
+  const renderRef = useRef(0);
+  const [renderCount, setRenderCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const renderRef=useRef(0)
-  const[count,setCount]=useState(0)
-  useEffect(()=>{
-    renderRef.current=renderRef.current+1
-  })
-const change=()=>{
-  setCount(count+1)
-    
-}
+  useEffect(() => {
+    renderRef.current += 1;
+    setRenderCount(renderRef.current);
+  }, [count]);
+
+  const change = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
-    <div>{renderRef.current} renders <br />
+    <div>
+      {renderCount} renders <br />
+      <h2>Render Counter</h2>
       {count} count <br />
       <button onClick={change}>re render</button>
     </div>
-  )
-}
+  );
+};
 
-export default Question4
+export default Question4;
