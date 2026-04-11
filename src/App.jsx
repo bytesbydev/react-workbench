12edd7f849b248de122ef554a1c62316754ef557
devendrarawat-code <devendrarawat9876@gmail.com>
2026-04-11 21:36:57 +0530

    made some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Question1 from './useState hook/Question1'
-import Question2 from './useState hook/Question2'
-import Question3 from './useState hook/Question3'
-import Question4 from './useState hook/Question4'
+import Question1 from './useeffect hook/Question1'
+import Question4 from './useeffect hook/Question4'
+import Question from './useState hook/Question4'
 import Question5 from './useState hook/Question5'
 import Question6 from './useState hook/Question6'
 import Question7 from './useState hook/Question7'
@@ -11,7 +10,7 @@ import Question7 from './useState hook/Question7'
 const App = () => {
   return (
     <div>
-      <Question7/>
+      <Question4/>
     </div>
   )
 }
