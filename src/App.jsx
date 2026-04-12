9ea6b5e8e1f90fc3227f40f58f9688e0d8ac72ff
devendrarawat-code <devendrarawat9876@gmail.com>
2026-04-12 22:54:29 +0530

    got some new things done in the folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import Question1 from './useeffect hook/Question1'
-import Question4 from './useeffect hook/Question4'
+import Question10 from './useeffect hook/Question10'
+import Question8 from './useeffect hook/Question8'
 import Question from './useState hook/Question4'
-import Question5 from './useState hook/Question5'
-import Question6 from './useState hook/Question6'
-import Question7 from './useState hook/Question7'
+
 
 
 const App = () => {
   return (
     <div>
-      <Question4/>
+      <Question10/>
     </div>
   )
 }
