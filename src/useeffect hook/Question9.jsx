369faa3970d44a51a87c0f08aea9868449300b85
bytesbydev <devendrarawat9876@gmail.com>
2026-05-08 23:07:50 +0530

    Update Question9.jsx

--- src/useeffect hook/Question9.jsx
+++ src/useeffect hook/Question9.jsx
@@ -1,17 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Question9 = () => {
-    const[num,setNum]=useState(1)
-    const[double,setDouble]=useState(2)
-    useEffect(()=>{
-        setDouble(prev=>prev*2)
-    },[num])
+  const [num, setNum] = useState(1);
+  const [double, setDouble] = useState(2);
+
+  useEffect(() => {
+    setDouble(num * 2);
+  }, [num]);
+
   return (
     <div>
-        <h2>{double}</h2> <br />
-        <button onClick={()=>setNum(num+1)}>Increase</button>
-    </div>
-  )
-}
+      <h2>{double}</h2>
 
-export default Question9
+      <br />
+
+      <button onClick={() => setNum(num + 1)}>
+        Increase
+      </button>
+    </div>
+  );
+};
+
+export default Question9;
