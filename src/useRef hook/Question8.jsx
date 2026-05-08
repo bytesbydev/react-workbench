93c627df1515ef00bbc5c43b182bc894aa91b7ed
bytesbydev <devendrarawat9876@gmail.com>
2026-05-08 23:06:26 +0530

    Update Question8.jsx

--- src/useRef hook/Question8.jsx
+++ src/useRef hook/Question8.jsx
@@ -1,20 +1,45 @@
-import React, { useMemo, useState } from 'react'
-const names=["anshika","dev","aloo","alia","aaraav","vicku","rohit"]
+import React, { useMemo, useState } from "react";
+
+const names = [
+  "anshika",
+  "dev",
+  "aloo",
+  "alia",
+  "aaraav",
+  "vicku",
+  "rohit",
+];
+
 const Question8 = () => {
-  const[text,setText]=useState("")
-  const result=useMemo(()=>{
-    console.log("filter")
-    const ans=names.filter((n)=>n.startsWith("a")) 
-  return ans
-  },[])
+  const [text, setText] = useState("");
+
+  const result = useMemo(() => {
+    console.log("filtering...");
+
+    return names.filter((n) =>
+      n.toLowerCase().startsWith(text.toLowerCase())
+    );
+  }, [text]);
+
   return (
     <div>
-      {result.map((num,index)=><p key={index}>{num}</p>)} <br />
-      text: {text} <br />
-<input type="text"onChange={(e)=>setText(e.target.value)} />
+      {result.map((num, index) => (
+        <p key={index}>{num}</p>
+      ))}
+
+      <br />
+
+      text: {text}
+
+      <br />
+
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Question8
-
+export default Question8;
