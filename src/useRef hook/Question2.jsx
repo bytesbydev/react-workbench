98ac0b909acd23c73d3acb07368fa542800d6cab
bytesbydev <devendrarawat9876@gmail.com>
2026-05-12 22:01:44 +0530

    Update Question2.jsx

--- src/useRef hook/Question2.jsx
+++ src/useRef hook/Question2.jsx
@@ -1,15 +1,38 @@
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
 
 const Question2 = () => {
-    const myref=useRef(0)
+
+  const countRef = useRef(0);
+
+  const increaseCount = () => {
+
+    countRef.current = countRef.current + 1;
+
+    console.log("Count :", countRef.current);
+  };
 
   return (
-    <div>
-        <button onClick={()=>{myref.current=myref.current+1
-            console.log(myref.current)
-        }}>change</button>
-    </div>
-  )
-}
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
 
-export default Question2
+      <h2>useRef Counter Example</h2>
+
+      <button
+        onClick={increaseCount}
+        style={{
+          padding: "10px 20px",
+          border: "none",
+          borderRadius: "8px",
+          background: "#4CAF50",
+          color: "white",
+          cursor: "pointer",
+          fontSize: "16px",
+        }}
+      >
+        Increase
+      </button>
+
+    </div>
+  );
+};
+
+export default Question2;
