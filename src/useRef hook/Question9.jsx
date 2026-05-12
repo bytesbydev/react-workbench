3c088ff58f35c453a2bc91a4d498c2bd57a706f7
bytesbydev <devendrarawat9876@gmail.com>
2026-05-12 21:58:41 +0530

    Update Question9.jsx

--- src/useRef hook/Question9.jsx
+++ src/useRef hook/Question9.jsx
@@ -1,26 +1,27 @@
-import React,{useMemo, useState} from 'react'
+import React, { useMemo, useState } from "react";
 
-const numbers=[34,32,55,34,23,54,35,34,34,76,34]
 const Question9 = () => {
-  const[count,setCount]=useState(0)
-  const result=useMemo(() => {
-console.log("calculating")
-let ans=0
-numbers.forEach(num => {
-  ans+=num
-}
-);
-return ans
-  }
-    , [numbers])
+  const [count, setCount] = useState(0);
+
+  const numbers = [34, 32, 55, 34, 23, 54, 35, 34, 34, 76, 34];
+
+  const result = useMemo(() => {
+    console.log("Calculating...");
+
+    return numbers.reduce((acc, curr) => acc + curr, 0);
+  }, []);
+
   return (
     <div>
-      {result}
-      <br />
-      count:{count}
-      <button onClick={()=>setCount(count+1)}>click me</button>
-    </div>
-  )
-}
+      <h2>Sum : {result}</h2>
 
-export default Question9
+      <h3>Count : {count}</h3>
+
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
+    </div>
+  );
+};
+
+export default Question9;
