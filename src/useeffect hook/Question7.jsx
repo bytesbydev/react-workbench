d77d3455be189b9e2d539380d507192b88257189
bytesbydev <devendrarawat9876@gmail.com>
2026-05-25 23:42:43 +0530

    Update Question7.jsx

--- src/useeffect hook/Question7.jsx
+++ src/useeffect hook/Question7.jsx
@@ -1,16 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Question7 = () => {
-    const[count,setCount]=useState(0)
-    useEffect(()=>{
-        document.title=`Count is ${count}`
-    },[count])
-  return (
-    <div><button style={{padding:"10px",borderRadius:"5px",backgroundColor:"orange"}} onClick={()=>setCount(count+1)}>Click me </button>
-    <br /> <br />
-     Clicked the button {count} times <br />
-     See at the title </div>
-  )
-}
+  const [count, setCount] = useState(0);
 
-export default Question7
+  useEffect(() => {
+    document.title = `Count is ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      <button
+        style={{
+          padding: "10px",
+          borderRadius: "5px",
+          backgroundColor: "orange",
+          border: "none",
+          cursor: "pointer",
+        }}
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Click Me
+      </button>
+
+      <br />
+      <br />
+
+      Clicked the button {count} times
+      <br />
+      See the title
+    </div>
+  );
+};
+
+export default Question7;
