46e208c035dee8be502d85784cc044fdbc02d0fd
devendrarawat-code <devendrarawat9876@gmail.com>
2026-06-03 21:45:59 +0530

    made some minor changes

--- src/useeffect hook/Question4.jsx
+++ src/useeffect hook/Question4.jsx
@@ -1,25 +1,41 @@
-import React, { useState,useEffect } from 'react'
-const names=["rerr","erwr","sdfsa","safas","kdfjdk","nsdfjsh"]
-const Question4 = () => {
-    const [text, settext] = useState("")
-    const [result,setResult]=useState(names)
-    useEffect(()=>{
-const timer=setTimeout(()=>{
-    setResult(names.filter(name=>name.toLowerCase().includes(text.toLowerCase())))
-},500)
+import React, { useEffect, useState } from "react";
 
- return()=>clearTimeout(timer)
-    },[text])
+const names = ["rerr", "erwr", "sdfsa", "safas", "kdfjdk", "nsdfjsh"];
+
+const Question4 = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState(names);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const filtered = names.filter((name) =>
+        name.toLowerCase().includes(text.toLowerCase())
+      );
+
+      setResult(filtered);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [text]);
+
   return (
     <div>
-        <label htmlFor="text">Enter the text to be searched: </label> <br />
-        <input type="text" onChange={(e)=>settext(e.target.value)} value={text} /> <br />
-         <h3>Results: </h3>
-        {result.map((value,index)=>(
-            <p key={index}>{value}</p>
-        ))}
-    </div>
-  )
-}
+      <label>Enter text to search:</label>
+      <br />
 
-export default Question4
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <h3>Results:</h3>
+
+      {result.map((value, index) => (
+        <p key={index}>{value}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Question4;
